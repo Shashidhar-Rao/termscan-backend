53db42d71e7aee174a3f15a7803d1675ad572c7e
Shashidhar-Rao <shashidhar.selfdev@gmail.com>
2026-03-31 01:18:13 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
@@ -20,5 +21,5 @@ def fetch_terms():
         return jsonify({ 'error': str(e) }), 500
 
 if __name__ == '__main__':
-    app.run()
-```
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
